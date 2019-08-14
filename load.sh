2562f4a80d91f2c6a88f20075467e12d11bbc2e3
juhani.kataja <juhani.kataja@csc.fi>
2019-08-14 14:02:51 +0300

    instructions and load.sh

---
+++ load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "Usage: load.sh <url>"
+  exit 1
+fi
+
+# Modify sleeptime for more cpu load
+sleeptime=0.23
+
+for i in {1..5000}
+do
+   curl $1 && echo "" &
+   echo "Curled $1 $i times. Sleeping $sleeptime"
+   sleep $sleeptime
+done
